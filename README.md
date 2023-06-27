8583c466f23ee832e3949bc8fe2e6d0ba4ef57b3
qlin960618 <qlin1806@gmail.com>
2023-06-27 22:42:56 +0900

    fix pkg name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# qlin_camera_control ROS package
+# qlin_ms_camera_processing ROS package
 
 Launch definition and kinematics scripts for pathology tele-operation
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['qlin_camera_processing'],
+    packages=['qlin_ms_camera_processing'],
     package_dir={'': 'scripts',
                  },
 )
